c14a25e7b30890a7ba4efb4d7975ae37153b91eb
lishuang <lishuang@tebon.com.cn>
2018-07-11 19:16:02 +0800

    trading 第二个

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int count = 1;
+    const int MAX_LOOP = 10000*10000;
+    std::cout<<"Hello World"<<std::endl;
 }
